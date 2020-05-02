429558e7c85f381e35718999071ddfac857edab4
Unknown <scherermarcel84@gmail.com>
2020-05-02 22:35:55 +0200

    Notification on Service
    notification on service works
    bugfix with add and set

--- app/app/src/main/java/com/scherer/garage/MainActivity.java
+++ app/app/src/main/java/com/scherer/garage/MainActivity.java
@@ -1,7 +1,6 @@
 package com.scherer.garage;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Context;
@@ -9,11 +8,12 @@ import android.content.Intent;
 import android.icu.util.Calendar;
 import android.icu.util.GregorianCalendar;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
-    static int HOURS   = 21;
-    static int MINUTES = 0;
+    public static int HOURS   = 17;
+    public static int MINUTES = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,26 +30,28 @@ public class MainActivity extends AppCompatActivity {
         alarm.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
     }
 
-    Calendar set_calendar_timer(int hours, int minutes){
-        Calendar cur_cal = new GregorianCalendar();
+    public static Calendar set_calendar_timer(int hours, int minutes){
+        Calendar cur_cal =  Calendar.getInstance();
         Calendar cal = new GregorianCalendar();
         //set the current time and date for this calendar
         cur_cal.setTimeInMillis(System.currentTimeMillis());
+        cal.set(Calendar.DAY_OF_YEAR, cur_cal.get(Calendar.DAY_OF_YEAR));
         if(cur_cal.get(Calendar.HOUR_OF_DAY) > hours){
-            cal.add(Calendar.DAY_OF_YEAR, cur_cal.get(Calendar.DAY_OF_YEAR) + 1);
+            cal.add(Calendar.DAY_OF_YEAR, 1);
         }else if(    cur_cal.get(Calendar.HOUR_OF_DAY) == hours
-                  && cur_cal.get(Calendar.MINUTE) > minutes){
-            cal.add(Calendar.DAY_OF_YEAR, cur_cal.get(Calendar.DAY_OF_YEAR) + 1);
-        }else{
-            cal.add(Calendar.DAY_OF_YEAR, cur_cal.get(Calendar.DAY_OF_YEAR));
+                  && cur_cal.get(Calendar.MINUTE) >= minutes) {
+            cal.add(Calendar.DAY_OF_YEAR, 1);
         }
         cal.set(Calendar.HOUR_OF_DAY, hours);
         cal.set(Calendar.MINUTE, minutes);
-        cal.set(Calendar.SECOND, cur_cal.get(Calendar.SECOND));
-        cal.set(Calendar.MILLISECOND, cur_cal.get(Calendar.MILLISECOND));
-        cal.set(Calendar.DATE, cur_cal.get(Calendar.DATE));
-        cal.set(Calendar.MONTH, cur_cal.get(Calendar.MONTH));
+        cal.set(Calendar.SECOND, 0);
+        cal.set(Calendar.YEAR, cur_cal.get(Calendar.YEAR));
 
+        Log.d("Scherer","set new alarm: " );
+        Log.d("Scherer","Day: " + Integer.toString(cal.get(Calendar.DAY_OF_MONTH)));
+        Log.d("Scherer","Month: " + Integer.toString(cal.get(Calendar.MONTH)+1));
+        Log.d("Scherer","Hour: " + Integer.toString(cal.get(Calendar.HOUR_OF_DAY)));
+        Log.d("Scherer","Minutes: " + Integer.toString(cal.get(Calendar.MINUTE)));
         return cal;
     }
 }
--- app/app/src/main/java/com/scherer/garage/MyUpdateService.java
+++ app/app/src/main/java/com/scherer/garage/MyUpdateService.java
@@ -1,24 +1,69 @@
 package com.scherer.garage;
 
+import android.app.AlarmManager;
 import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.icu.util.Calendar;
+import android.icu.util.GregorianCalendar;
+import android.os.Build;
 import android.os.IBinder;
 
 import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 public class MyUpdateService extends Service {
 
+    String CHANNEL_ID = "SchererNotification";
+
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        // This is the Notification Channel ID. More about this in the next section
-        String NOTIFICATION_CHANNEL_ID = "channel_id";
-        //Notification Channel ID passed as a parameter here will be ignored for all the Android versions below 8.0
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID);
-        builder.setContentTitle("This is heading");
-        builder.setContentText("This is description");
-        Notification notification = builder.build();
+        createNotificationChannel();
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.garage)
+                .setContentTitle("Garage Monitor")
+                .setContentText("The garage is open yet ...")
+                .setVibrate(new long[] { 1000, 1000, 1000, 1000, 1000 })
+                .setLights(Color.RED, 3000, 3000)
+                .setPriority(NotificationCompat.PRIORITY_HIGH);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+
+        int notificationId = 12348;
+        // notificationId is a unique int for each notification that you must define
+        notificationManager.notify(notificationId, builder.build());
+
+        Intent myIntent = new Intent(MyUpdateService.this, MyUpdateService.class);
+        PendingIntent  pendingIntent = PendingIntent.getService(MyUpdateService.this, 0, myIntent, 0);
+        Calendar calendar = new GregorianCalendar();
+        // set alarm time
+        calendar = MainActivity.set_calendar_timer(MainActivity.HOURS, MainActivity.MINUTES);
+        AlarmManager alarm = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        // Start service every day at 21:00
+        alarm.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
         return Service.START_NOT_STICKY;
+    }
+
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "ChanelSchererName";
+            String description = "CahenelSchererDescription";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
     }
 
     @Override
